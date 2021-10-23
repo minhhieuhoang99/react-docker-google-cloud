a09b35b34f3efbd03f00e16a54f2dcc8c766c725
minhhieuhoang99 <57648053+minhhieuhoang99@users.noreply.github.com>
2021-10-23 17:59:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="http://mango.viecrew.com/wp-content/uploads/2021/03/mangoooo.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Mango react-docker-google-cloud
         </p>
         <a
           className="App-link"
